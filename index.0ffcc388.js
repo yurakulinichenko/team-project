9d246f207a981d4bdc9e9d02a1d7bcd6e0cdd3c9
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-17 21:20:33 +0000

    Deploying to gh-pages from @ yurakulinichenko/team-project@235ad0a17188d3710b78f290041ac659206dfcf7 🚀

---
+++ index.0ffcc388.js
@@ -0,0 +1,2 @@
+!function(){var e;e=document.querySelector(".button-to-top"),window.addEventListener("scroll",(function(t){window.pageYOffset>500?e.style.display="flex":e.style.display="none"}))}();
+//# sourceMappingURL=index.0ffcc388.js.map
