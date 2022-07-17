81f40eb176b986e3e90cce4bf1af067dea5768cc
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-17 14:48:18 +0300

    Update slider.js

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,8 +1,4 @@
 var $jq = jQuery.noConflict();
 $jq(document).ready(function () {
   $jq('.customer').slick();
-  $jq('a').click(function (event) {
-    $jq(this).hide();
-    event.preventDefault();
-  });
 });
