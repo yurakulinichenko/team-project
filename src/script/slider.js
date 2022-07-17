a354fd5e3b1be827cc36a660b2d7a91af87061b0
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-17 09:00:06 +0300

    added main

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,12 +1,35 @@
-$(function () {
-  $('.customer').slick();
-});
-{
-  /* <script type="text/javascript">
-$(document).ready(function(){
-  $('.customer').slick({
-    setting-name: setting-value
-  });
-});
-</script> */
+// var $jq = jQuery.noConflict();
+// $jq(document).ready(function () {
+//   $jq('a').click(function (event) {
+//     $jq(this).hide();
+//     event.preventDefault();
+//   });
+// });
+
+// $(document).ready(function () {
+//   $('.customer').slick();
+// });
+
+function initSlider() {
+  if ($.fn.slick) {
+    $('.customer').slick();
+  } else {
+    setTimeout(function () {
+      initSlider();
+    }, 500);
+  }
 }
+
+// const a = () => {
+//   document.getElementsByClassName('customer').slick();
+// };
+// a();
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   // инициализация слайдера
+//   const slider = new SimpleAdaptiveSlider('.slider-items', {
+//     autoplay: false,
+//     interval: 5000,
+//     swipe: true,
+//   });
+// });
