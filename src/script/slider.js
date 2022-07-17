a640e76d95556181a057b5b4a0efa6082f4cac10
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-17 14:08:01 +0300

    added slider

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,36 +1,8 @@
-// var $jq = jQuery.noConflict();
-// $jq(document).ready(function () {
-//   $jq('a').click(function (event) {
-//     $jq(this).hide();
-//     event.preventDefault();
-//   });
-// });
-
-// $(document).ready(function () {
-//   $('.customer').slick();
-// });
-
-// function initSlider() {
-//   if ($.fn.slick) {
-//     $('.customer').slick();
-//   } else {
-//     setTimeout(function () {
-//       initSlider();
-//     }, 500);
-//   }
-// }
-
-// const a = () => {
-//   document.getElementsByClassName('customer').slick();
-// };
-// a();
-
-document.addEventListener('DOMContentLoaded', () => {
-  // инициализация слайдера
-  new ItcSimpleSlider('.itcss', {
-    loop: true,
-    autoplay: false,
-    interval: 5000,
-    swipe: true,
+var $jq = jQuery.noConflict();
+$jq(document).ready(function () {
+  $jq('.customer').slick();
+  $jq('a').click(function (event) {
+    $jq(this).hide();
+    event.preventDefault();
   });
 });
