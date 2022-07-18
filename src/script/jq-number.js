7dd1af0b515f4c190b0ece7466d09e53e61308a2
JarinaDev <jarinka91@gmail.com>
2022-07-18 22:12:50 +0300

    done

--- src/script/jq-number.js
+++ src/script/jq-number.js
@@ -1,12 +1,12 @@
 var show = true;
 var countbox = ".advantages";
 jQuery(window).on("scroll load resize", function () {
-    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-    var w_top = jQuery(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-    var e_top = jQuery(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-    var w_height = jQuery(window).height(); // Высота окна браузера
-    var d_height = jQuery(document).height(); // Высота всего документа
-    var e_height = jQuery(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (!show) return false;
+    var w_top = jQuery(window).scrollTop();
+    var e_top = jQuery(countbox).offset().top; 
+    var w_height = jQuery(window).height();
+    var d_height = jQuery(document).height();
+    var e_height = jQuery(countbox).outerHeight();
     if (w_top + 500 >= e_top ||  w_height + w_top == d_height || e_height + e_top < w_height) {
         jQuery(document).ready(function () {
             jQuery('.advantages__characteristic').css('opacity', '1');
