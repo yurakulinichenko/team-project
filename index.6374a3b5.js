2edcc802521bdd1ad83046395a408f65bde1b5ed
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-18 21:57:00 +0000

    Deploying to gh-pages from @ yurakulinichenko/team-project@1c2da0199b6a526db51c2ea18e5e094e604e524d 🚀

--- index.6374a3b5.js
+++
@@ -1,2 +0,0 @@
-var o=jQuery.noConflict();o(document).ready((function(){o(".customer").slick({arrows:!1,autoplay:!0,autoplaySpeed:3e3,dots:!0,dotsClass:"slider-dots"})}));
-//# sourceMappingURL=index.6374a3b5.js.map
--- index.ab8169b3.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=jQuery.noConflict();o(document).ready((function(){o(".customer").slick({arrows:!1,autoplay:!0,autoplaySpeed:3e3,dots:!0,dotsClass:"slider-dots"})}))}();
-//# sourceMappingURL=index.ab8169b3.js.map
