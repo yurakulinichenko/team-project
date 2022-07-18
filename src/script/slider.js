f49e1d56ff2af80404c0228b841f9f492f5f6288
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-18 15:46:25 +0300

    added styles to slider

--- src/script/slider.js
+++ src/script/slider.js
@@ -1,4 +1,10 @@
 var $jq = jQuery.noConflict();
 $jq(document).ready(function () {
-  $jq('.customer').slick();
+  $jq('.customer').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    dotsClass: 'slider-dots',
+  });
 });
