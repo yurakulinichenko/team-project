a00dda1c010bd835add8cf8ed6cb60baf50ffbc6
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-19 12:07:10 +0000

    Deploying to gh-pages from @ yurakulinichenko/team-project@1909cbbf9f7c7c1c72d257249c839e24f30641e1 🚀

---
+++ index.8298c90c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector(".about__button"),closeModalBtn:document.querySelector(".modal-button"),modal:document.querySelector(".about-modal"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.8298c90c.js.map
--- index.f7f3d509.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector(".about__button"),closeModalBtn:document.querySelector(".modal-button"),modal:document.querySelector(".about-modal")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.f7f3d509.js.map
